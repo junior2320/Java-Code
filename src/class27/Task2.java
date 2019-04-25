8d5660d0eb065dcb25853a0a7f9b6258f3e70093
junior2320 <quintanillajr2320@gmail.com>
2019-04-24 23:42:15 -0400

    code 27

---
+++ src/class27/Task2.java
@@ -0,0 +1,31 @@
+package class27;
+
+
+//	2. Write program: user class  has a constructor that takes 
+//	name and mobile number. Extend this class  by userInfo that 
+//	will have user address variable. Print users name, mobile 
+//	number and address in userDetails method. Test your code.
+//   public class Task2 {
+//	   String name;
+//	    long phone;
+//
+//	}
+//	class userInfo extends Task2 {
+//	    String address;
+//	    userInfo(String name, long phone, String address){
+//	        this.name=name;
+//	        this.phone=phone;
+//	        this.address=address;
+//	            }
+//	    void userDetails() {
+//	        System.out.println(name+" "+phone+" "+address);
+//	    }
+//	    public static void main (String[] args) {
+//	    	userInfo ChunLee= new userInfo("ChunLee", 93434342342341l,"California westside" );
+//	        ChunLee.userDetails();
+//	    }
+	//}
+		
+	
+
+
